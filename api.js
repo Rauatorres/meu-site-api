167dca974a478bddfdb67748a0210232f287ae45
rauatorres <rauatorres@gmail.com>
2023-09-04 23:05:31 -0300

    comando configs

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 const app = require('./configs/server')
 const dbconfig = require('./configs/database')
 
-var porta = 3001
+var porta = 3000
 
 app.listen(porta, ()=>{
   console.log(`Servidor rodando em localhost:${porta}`)
